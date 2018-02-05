f35fe21428711c16316601a4d0cbd8eb33627362
wojtek <wojtek.biniek@gmail.com>
2018-02-06 00:30:42 +0100

    auto_configure.sh improvements

--- auto_configure.sh
+++ auto_configure.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
 # configure vim
-git clone https://github.com/VundleVim/Vundle.vim.git vim/vim/bundle/Vundle.vim
-ln -s $(readlink -m vim/vim) ~/.vim
-ln -s $(readlink -m vim/vimrc) ~/.vimrc
+VIM_CONFIG_DIR=~/.vim/bundle/Vundle.vim
+VIM_RC_PATH=~/.vimrc
+
+rm -Rf ${VIM_CONFIG_DIR} ${VIM_RC_PATH}
+
+git clone https://github.com/VundleVim/Vundle.vim.git ${VIM_CONFIG_DIR}
+ln -s $(readlink -m vim/vimrc) ${VIM_RC_PATH}
+
+vim +PluginInstall +qall
